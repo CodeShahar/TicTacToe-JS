22b0ace1877fa1a79e9c92156c25b40efab3050e
head-node <sudhanshuoff@gmail.com>
2024-07-13 11:09:29 +0530

    Added animation and simplified the code

--- index.js
+++ index.js
@@ -1,113 +1,129 @@
-let inputValues = []
-let winningCombination = [
+const winningCombination = [
     [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7], [1, 4, 7], [2, 5, 8], [3, 6, 9]
-]
-let player1Name
-let player2Name
-const handleInputChange = (event, id) => {
-    console.log(event.target.value)
-    if (id == 'player1') player1Name = event.target.value
-    else player2Name = event.target.value
-}
-const savePlayersName = () => {
-    if (!player1Name || !player2Name) {
-        return
-    }
-    let player1NameElement = document.getElementById('player1Name')
-    let player2NameElement = document.getElementById('player2Name')
+];
+let player1Name, player2Name;
+let player1Moves = [], player2Moves = [];
+let winner = -1, current, initial, totalCount = 0;
 
-    switch (current) {
-        case 1: {
-            player1NameElement.textContent = player1Name + ' will play as X'
-            player2NameElement.textContent = player2Name + ' will play as O'
-            break;
-        }
-        case 2: {
-            player1NameElement.textContent += player1Name + ' will play as O'
-            player2NameElement.textContent += player2Name + ' will play as X'
-            break;
-        }
-    }
-    gridContainer.style.opacity = '1'
-    gridContainer.style.pointerEvents = 'all'
-    document.getElementById('profileContainer').style.display = 'none'
-}
-let winner = -1
 const gridContainer = document.getElementById('grid-container');
-gridContainer.style.opacity = '.1'
-gridContainer.style.pointerEvents = 'none'
-let intial = -1
-let whoWillStart = Math.floor(Math.random() * 2);
-let current = -1
-if (whoWillStart == 1) {
-    current = 1
-    initial = 'X'
+const winnerElement = document.getElementById('winner');
+const player1NameElement = document.getElementById('player1Name');
+const player2NameElement = document.getElementById('player2Name');
+const profileContainer = document.getElementById('profileContainer');
+const playAgainButton = document.getElementById('play-again')
+
+const handleInputChange = (event, id) => {
+    if (id == 'player1') player1Name = event.target.value;
+    else player2Name = event.target.value;
+};
+
+const savePlayersName = () => {
+    if (!player1Name || !player2Name) return;
+
+    const players = [
+        { name: player1Name, symbol: initial === 'X' ? 'X' : 'O' },
+        { name: player2Name, symbol: initial === 'X' ? 'O' : 'X' }
+    ];
+
+    player1NameElement.textContent = `${players[0].name} will play as ${players[0].symbol}`;
+    player2NameElement.textContent = `${players[1].name} will play as ${players[1].symbol}`;
+
+    gridContainer.style.opacity = '1';
+    gridContainer.style.pointerEvents = 'all';
+    profileContainer.style.display = 'none';
+};
+
+const playAgain = () =>{
+    // function to reset the boards and variables to reset the game for current players
+    player1Moves = [];
+    player2Moves = [];
+    totalCount = 0;
+    winner = -1;
+    winnerElement.textContent = '';
+    playAgainButton.style.display = 'none';
+    gridContainer.innerHTML = ''; 
+    initializeGame();
 }
-else {
-    current = 2
-    initial = 'O'
-}
 
+const initializeGame = () => {
+    // entry function to create board and selecting the initial player
+    gridContainer.style.opacity = '0.1';
+    gridContainer.style.pointerEvents = 'none'; 
+    current = Math.floor(Math.random() * 2) + 1;
+    initial = current === 1 ? 'X' : 'O';
 
-
-for (let row = 0; row < 3; row++) {
-    for (let col = 0; col < 3; col++) {
-        const gridItem = document.createElement('button');
-        gridItem.classList.add('grid-item');
-        // gridItem.textContent = `(${row }, ${col})`;
-        gridItem.addEventListener('click', () => handleClick(3 * row + ((col + 1)), gridItem))
-        gridContainer.appendChild(gridItem);
-    }
-}
-
-
-let player1Moves = []
-let player2Moves = []
-let winnerElement = document.getElementById('winner')
-let totalCount = 0
-
-
-function handleClick(val, element) {
-    ++totalCount
-    if (current == 1) {
-        if (initial == 'X')
-            player1Moves.push(val)
-        else
-            player2Moves.push(val)
-        element.textContent = 'X'
-        current = 2
-    }
-    else {
-        if (initial == 'O')
-            player1Moves.push(val)
-        else
-            player2Moves.push(val)
-        element.textContent = 'O'
-        current = 1
-    }
-    for (let i = 0; i < winningCombination.length; i++) {
-        if (isArraySubset(winningCombination[i], player1Moves)) {
-            winner = 1
-            console.log(winningCombination[i], player1Moves, 1);
-            winnerElement.textContent = `WooHoo, the Winner is Player${winner}`
-            gridContainer.style.pointerEvents = 'none'
-            break;
-        }
-        if (isArraySubset(winningCombination[i], player2Moves)) {
-            winner = 2
-            console.log(winningCombination[i], player2Moves, 2);
-            winnerElement.textContent = `WooHoo, the Winner is Player${winner}`
-            gridContainer.style.pointerEvents = 'none'
-            break;
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++) {
+            const gridItem = document.createElement('button');
+            gridItem.classList.add('grid-item');
+            gridItem.dataset.index = 3 * row + col + 1;
+            gridContainer.appendChild(gridItem);
         }
     }
-    element.style.cursor = 'not-allowed'
-    if (totalCount == 9 && winner == -1) {
-        winnerElement.textContent = 'You both are smart, try some tricks to win'
+};
+
+const handleClick = (event) => {
+    // function to handle the onClick event on gridItem
+    const element = event.target;
+    const val = parseInt(element.dataset.index, 10);
+
+    if (element.textContent || winner !== -1) return;
+
+    ++totalCount;
+
+    if (current === 1) {
+        if (initial === 'X') player1Moves.push(val);
+        else player2Moves.push(val);
+        element.textContent = 'X';
+        current = 2;
+    } else {
+        if (initial === 'O') player1Moves.push(val);
+        else player2Moves.push(val);
+        element.textContent = 'O';
+        current = 1;
     }
 
-}
+    if (checkWinner(player1Moves)) {
+        winner = 1;
+        winnerElement.textContent = `WooHoo, the Winner is ${player1Name}`;
+        gridContainer.style.pointerEvents = 'none';
+    }
 
-function isArraySubset(array1, array2) {
-    return array1.every(element => array2.includes(element));
-}
+    if (checkWinner(player2Moves)) {
+        winner = 2;
+        winnerElement.textContent = `WooHoo, the Winner is ${player2Name}`;
+        gridContainer.style.pointerEvents = 'none';
+    }
+
+    element.style.cursor = 'not-allowed';
+
+    if (totalCount === 9 && winner === -1) {
+        winnerElement.textContent = 'You both are smart, try some tricks to win';
+    }
+    if (totalCount == 9 || winner != -1) {
+        playAgainButton.style.display = 'block'
+
+    }
+};
+
+const checkWinner = (playerMoves) => {
+    // function to check whether player moves contains any possible winning combination
+    for (const combination of winningCombination) {
+        if (combination.every(cell => playerMoves.includes(cell))) {
+            highlightWinningCells(combination);
+            return true;
+        }
+    }
+    return false;
+};
+
+const highlightWinningCells = (winningCells) => {
+    // function to create animation for winning combination
+    for (const index of winningCells) {
+        const cell = gridContainer.querySelector(`[data-index='${index}']`);
+        cell.classList.add('winning');
+    }
+};
+
+gridContainer.addEventListener('click', handleClick);
+initializeGame();
